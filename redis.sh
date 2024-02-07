8872c807b16a47f97e93796ca1395592c1a51256
mohammad9haseeb <mohammad9haseeb@gmail.com>
2024-02-07 05:00:26 -0500

    4th iteration correcting thw SED command

--- redis.sh
+++ redis.sh
@@ -6,7 +6,7 @@ dnf module enable redis:remi-6.2 -y
 dnf install redis -y
 
 echo -e "\e[36m>>>>>change listen port<<<<\e[0m"
-sed -i -e 's|127.0.0.0|0.0.0.0' /etc/redis/redis.conf /etc/redis.conf
+sed -i -e "s|127.0.0.0|0.0.0.0" /etc/redis/redis.conf /etc/redis.conf
 
 echo -e "\e[36m>>>>>start service<<<<\e[0m"
 systemctl enable redis
